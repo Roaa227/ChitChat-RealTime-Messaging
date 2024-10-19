af355fd3b3f9bedcfae133e67ee83cd60624b9cf
Rana <ranamohamad215@gmail.com>
2024-10-19 22:32:18 +0300

    Added User authentication.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import MainLayout from './components/MainPage/MainPage'
-import React from 'react';
+import MainLayout from "./components/MainPage/MainPage";
+import React from "react";
 import {
+  Navigate,
   Route,
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
-} from 'react-router-dom';
-import { Home } from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import Signup from './pages/Signup/Signup';
-
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-    <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path="/chat" element={<MainLayout />} />
-    </>
-
-  )
-);
+} from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import { useAuthContext } from "./contexts/authContext";
 
 function App() {
+  const { authUser } = useAuthContext();
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <Signup />}
+        />
+        <Route path="/chat" element={<MainLayout />} />
+      </>
+    )
+  );
   return <RouterProvider router={router} />;
 }
 
