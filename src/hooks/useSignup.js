b9e11082a4d2dd888c20f6d7d942d4f53811bbff
Rana <ranamohamad215@gmail.com>
2024-10-19 22:33:19 +0300

    Created Signup hook

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { useAuthContext } from "../contexts/authContext";
+
+const validRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+let validInputs = true;
+
+const useSignup = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+  const signup = async (formData, setErrors) => {
+    const { userName, email, password } = formData;
+    handleInputErrors(formData, setErrors);
+    if (!validInputs) return;
+
+    setLoading(true);
+    try {
+      const res = await fetch(`${import.meta.env.VITE_Back_END_Host}/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ userName, email, password }),
+      });
+
+      console.log("Kellow");
+      const data = await res.json();
+      if (data.errors) {
+        setErrors(data.errors);
+        return;
+      }
+      console.log(data);
+
+      // Save token:
+      localStorage.setItem("authUser", JSON.stringify(data.token));
+      setAuthUser(data.token);
+
+    } catch (error) {
+      // Handle any other unexpected errors that may occur
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, signup };
+};
+
+export default useSignup;
+
+function handleInputErrors(formData, setErrors) {
+  const { userName, email, password } = formData;
+  const newErrors = { userName, email, password };
+  if (!userName) {
+    validInputs = false;
+    newErrors.userName = "Enter a user name.";
+  }
+  if (!email || !email.match(validRegex)) {
+    validInputs = false;
+    newErrors.email = "Enter a valid email.";
+  }
+  if (!password || password.length < 8) {
+    validInputs = false;
+    newErrors.password = "Password can't be less than 8 characters.";
+  }
+
+  setErrors(newErrors);
+}
