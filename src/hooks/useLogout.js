3de2e2ed205ec1d9821c3febe237629fd1de181f
Rana <ranamohamad215@gmail.com>
2024-10-20 13:12:44 +0300

    Fixed an error.

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,31 +1,34 @@
-import { useState } from "react"
+import { useState } from "react";
 import { useAuthContext } from "../contexts/authContext";
 
-
 function useLogout() {
-
   const [loading, setLoading] = useState(false);
-  const {setAuthUser} = useAuthContext();
-   const logout = async ()=>{
+  const { setAuthUser } = useAuthContext();
+  const logout = async () => {
     setLoading(true);
     try {
-        const res = await fetch(`${import.meta.env.VITE_Back_END_Host}/logout`, {
-            method: "POST",
-            headers: {"Content-Type": "application/json"}
-        });
-        const data = await res.json();
-        if(data.error){
-            throw new Error(data.error);
-        }
-        localStorage.removeItem("authUser");
-        setAuthUser(null);
+      
+      const res = await fetch(`${import.meta.env.VITE_Back_END_Host}/logout`, {
+        method: "GET", // Use GET to match the backend
+        headers: { "Content-Type": "application/json" },
+        credentials: "include", // This ensures cookies are sent
+      });
+      const data = await res.json();
+      
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      
+
+      localStorage.removeItem("authUser");
+      setAuthUser(null);
     } catch (error) {
-        alert(error.message);
-    }finally{
-        setLoading(false);
+      alert(error.message);
+    } finally {
+      setLoading(false);
     }
-   }
-   return {loading, logout};
+  };
+  return { loading, logout };
 }
 
-export default useLogout
+export default useLogout;
