9fe03edb1b524a0d5adf136be28380f4cb0f4905
Rana <ranamohamad215@gmail.com>
2024-10-22 00:46:46 +0300

    Added conversation filters

--- src/hooks/useGetChats.js
+++ src/hooks/useGetChats.js
@@ -1,24 +1,31 @@
 import { useEffect, useState } from "react";
 import { useAuthContext } from "../contexts/authContext";
 
-const useGetChats = () => {
+const useGetChats = (filterType, searchQuery = "") => {
   const [loading, setLoading] = useState(false);
   const [chats, setChats] = useState([]);
   const { authUser } = useAuthContext();
 
   useEffect(() => {
+    let url = "";
+    if (searchQuery) {
+      url = `/search/:${searchQuery}`;
+    } else if (filterType === "group") {
+      url = "/filterGroups";
+    } else if (filterType === "individual") {
+      url = "/filterUsers";
+    } else {
+      url = `/sidebarUsers`;
+    }
     const getChats = async (signal) => {
       if (!authUser) return;
       setLoading(true);
       try {
-        const res = await fetch(
-          `${import.meta.env.VITE_Back_END_Host}/sidebarUsers`,
-          {
-            method: "GET",
-            credentials: "include",
-            signal, // pass abort signal
-          }
-        );
+        const res = await fetch(`${import.meta.env.VITE_Back_END_Host}${url}`, {
+          method: "GET",
+          credentials: "include",
+          signal, // pass abort signal
+        });
         const data = await res.json();
         if (data.error) {
           throw new Error(data.error);
@@ -41,7 +48,7 @@ const useGetChats = () => {
     return () => {
       controller.abort(); // Cleanup function to cancel the previous request if the component re-renders
     };
-  }, [authUser]);
+  }, [authUser, filterType, searchQuery]);
 
   return { loading, chats };
 };
