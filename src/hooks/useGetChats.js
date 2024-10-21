b289bd51e925460dbb02703746d899667e1a50dd
Rana <ranamohamad215@gmail.com>
2024-10-21 22:38:13 +0300

    Edited the use effect function

--- src/hooks/useGetChats.js
+++ src/hooks/useGetChats.js
@@ -5,33 +5,37 @@ const useGetChats = () => {
   const [loading, setLoading] = useState(false);
   const [chats, setChats] = useState([]);
   const { authUser } = useAuthContext();
-  useEffect(() => {
-    const getChats = async () => {
-      if (!authUser) return;
-      setLoading(true);
-      try {
-        const res = await fetch(
-          `${import.meta.env.VITE_Back_END_Host}/sidebarUsers`,
-          {
-            method: "GET",
-            credentials: "include",
-          }
-        );
-        const data = await res.json();
-        if (data.error) {
-          throw new Error(data.error);
+  const getChats = async () => {
+    if (!authUser) return;
+    setLoading(true);
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_Back_END_Host}/sidebarUsers`,
+        {
+          method: "GET",
+          credentials: "include",
         }
-        setChats(data);
-      } catch (error) {
-        // alert(error.message);
-        console.log(error.message);
-      } finally {
-        setLoading(false);
+      );
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
       }
-    };
-
+      console.log("From useGetChats: 👴🏼👴🏼👴🏼👴🏼", data);
+      setChats(data);
+    } catch (error) {
+      // alert(error.message);
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    const controller = new AbortController();
     getChats();
-  }, [authUser]);
+    return () => {
+      controller.abort(); // Cleanup function to cancel the previous request if the component re-renders
+    };
+  }, []);
 
   return { loading, chats };
 };
