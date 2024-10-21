ac60c266f9eee34b45089023209179489dd1c776
Rana <ranamohamad215@gmail.com>
2024-10-21 22:50:59 +0300

    Abdded abort to cancel repeated fetch.

--- src/hooks/useGetChats.js
+++ src/hooks/useGetChats.js
@@ -5,37 +5,43 @@ const useGetChats = () => {
   const [loading, setLoading] = useState(false);
   const [chats, setChats] = useState([]);
   const { authUser } = useAuthContext();
-  const getChats = async () => {
-    if (!authUser) return;
-    setLoading(true);
-    try {
-      const res = await fetch(
-        `${import.meta.env.VITE_Back_END_Host}/sidebarUsers`,
-        {
-          method: "GET",
-          credentials: "include",
-        }
-      );
-      const data = await res.json();
-      if (data.error) {
-        throw new Error(data.error);
-      }
-      console.log("From useGetChats: 👴🏼👴🏼👴🏼👴🏼", data);
-      setChats(data);
-    } catch (error) {
-      // alert(error.message);
-      console.log(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+
   useEffect(() => {
+    const getChats = async (signal) => {
+      if (!authUser) return;
+      setLoading(true);
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_Back_END_Host}/sidebarUsers`,
+          {
+            method: "GET",
+            credentials: "include",
+            signal, // pass abort signal
+          }
+        );
+        const data = await res.json();
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        console.log("From useGetChats: 👴🏼👴🏼👴🏼👴🏼", data);
+        setChats(data);
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          console.log(error.message);
+        }
+        // alert(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
     const controller = new AbortController();
-    getChats();
+    if (authUser) {
+      getChats(controller.signal);
+    }
     return () => {
       controller.abort(); // Cleanup function to cancel the previous request if the component re-renders
     };
-  }, []);
+  }, [authUser]);
 
   return { loading, chats };
 };
