d7066ae5398415d37b0b4956ce06de44d4d34bae
Rana <ranamohamad215@gmail.com>
2024-10-22 22:21:53 +0300

    created socket functionality

---
+++ src/hooks/useSendMessage.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import {useChatContext} from "../contexts/ChatContext";
+const useSendMessage = () => {
+  const [loading, setLoading] = useState();
+  const { messages, setMessages, selectedContact } = useChatContext();
+
+  const sendMessage = async (message) => {
+    setLoading(true);
+    const payload = { message: message, receiverIds: [selectedContact._id] };
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_Back_END_Host}/sendMessage`,
+        {
+          method: "POST",
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(payload),
+        }
+      );
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      console.log("the message: 🥳", data);
+      setMessages([...messages, data]);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { sendMessage, loading };
+};
+
+export default useSendMessage;
