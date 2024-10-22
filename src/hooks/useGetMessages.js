ed59b7e6626f040f427c66612cb157f09d4f0a41
Rana <ranamohamad215@gmail.com>
2024-10-22 22:21:20 +0300

    Added socket functionality.

---
+++ src/hooks/useGetMessages.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { useChatContext } from "../contexts/ChatContext";
+
+
+const useGetMessages = () => {
+  const [loading, setLoading] = useState(false);
+    const {messages, setMessages, selectedContact } = useChatContext()
+
+ useEffect(()=>{
+    const getMessages = async ()=>{
+        setLoading(true);
+        try {
+            const res = await fetch(`${import.meta.env.VITE_Back_END_Host}/getMessage`,{
+                method: "POST",
+                headers:{
+                    'Content-Type':"application/json",
+                },
+                credentials:"include",
+                body:JSON.stringify({userToChatIds:selectedContact._id})
+            })
+            const data = await res.json();
+            if (data.error){
+                throw new Error(data.error)
+            }
+            setMessages(data);
+        } catch (error) {
+            alert(error.messages);
+        }finally {
+            setLoading(false);
+        }
+    }
+    if(selectedContact?._id) getMessages()
+ },[selectedContact._id, setMessages])
+return {messages, loading}
+}
+export default useGetMessages
---
+++ src/hooks/useListenMessages.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useChatContext } from "../contexts/ChatContext";
+import { useSocketContext } from "../contexts/SocketContext";
+
+const useListenMessages = () => {
+  const { socket } = useSocketContext();
+  const { messages, setMessages } = useChatContext();
+
+  useEffect(() => {
+    socket?.on("newMessage", (newMessage) => {
+      setMessages((prevMess) => [...prevMess, newMessage]);
+    });
+
+    return () => socket.off("newMessage");
+  }, [socket, setMessages]);
+};
+
+export default useListenMessages;
