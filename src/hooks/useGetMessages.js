92f02552eca38572aa296e4789c3d38e8935207e
Rana <ranamohamad215@gmail.com>
2024-10-23 00:03:20 +0300

    fixed a bug

--- src/hooks/useGetMessages.js
+++ src/hooks/useGetMessages.js
@@ -1,36 +1,39 @@
 import { useEffect, useState } from "react";
 import { useChatContext } from "../contexts/ChatContext";
 
-
 const useGetMessages = () => {
   const [loading, setLoading] = useState(false);
-    const {messages, setMessages, selectedContact } = useChatContext()
+  const { messages, setMessages, selectedContact } = useChatContext();
 
- useEffect(()=>{
-    const getMessages = async ()=>{
-        setLoading(true);
-        try {
-            const res = await fetch(`${import.meta.env.VITE_Back_END_Host}/getMessage`,{
-                method: "POST",
-                headers:{
-                    'Content-Type':"application/json",
-                },
-                credentials:"include",
-                body:JSON.stringify({userToChatIds:selectedContact._id})
-            })
-            const data = await res.json();
-            if (data.error){
-                throw new Error(data.error)
-            }
-            setMessages(data);
-        } catch (error) {
-            alert(error.messages);
-        }finally {
-            setLoading(false);
+  useEffect(() => {
+    const getMessages = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_Back_END_Host}/getMessage`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify({ userToChatIds: selectedContact._id }),
+          }
+        );
+        const data = await res.json();
+        if (data.error) {
+          throw new Error(data.error);
         }
-    }
-    if(selectedContact?._id) getMessages()
- },[selectedContact._id, setMessages])
-return {messages, loading}
-}
-export default useGetMessages
+        setMessages(data);
+        console.log(data);
+      } catch (error) {
+        alert(error.messages);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (selectedContact?._id) getMessages();
+  }, [selectedContact._id, setMessages]);
+  return { messages, loading };
+};
+export default useGetMessages;
