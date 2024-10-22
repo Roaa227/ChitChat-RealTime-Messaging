0627dcbd1d368dd1ee2edb45104cb3f04379bab8
Rana <ranamohamad215@gmail.com>
2024-10-22 22:20:26 +0300

    removed unnecessary import.

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+
 import logo from '/logoo.png';
 
 
 export const Topbar = () => {
     return (
-  <nav class="navbar" style={{ backgroundColor:'#DFDCF2', borderRight:'1px solid #9049bf' }}>
-        <div class="container-fluid">
-          <a class="navbar-brand" style={{color:'#9049BF' , backgroundColor:'#DFDCF2'}} href="#">
-            <img src={logo} alt="Logo" width="10%" class="d-inline-block align-text-top me-2" />
+  <nav className ="navbar" style={{ backgroundColor:'#DFDCF2', borderRight:'1px solid #9049bf' }}>
+        <div className ="container-fluid">
+          <a className="navbar-brand" style={{color:'#9049BF' , backgroundColor:'#DFDCF2'}} href="#">
+            <img src={logo} alt="Logo" width="10%" className="d-inline-block align-text-top me-2" />
             CHITCHAT
           </a>
         </div>
